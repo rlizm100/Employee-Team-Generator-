8a93e604727e339183277d0f8ff2d00b0b1972b2
rlizm100 <rlizm100@cox.net>
2020-06-30 13:42:45 -0400

    second commit

--- app.js
+++ app.js
@@ -16,68 +16,28 @@ function startPrompt() {
 startPrompt()
 
 const employeeInfo = [];
-
-function newTeamMember() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "addTeamMember",
-            message: "what type of team member would you like to add",
-            choices: ["Engineer", "Intern", "I don't want to add anymore"]
-        }
-    ])
-        .then(userInput => {
-            if (userInput.addTeamMember === "Engineer") {
-                addEngineer();
-            }
-            else if (userInput.addTeamMember === "Intern") {
-                addIntern();
-            }
-            else if (userInput.addTeamMember === "I don't want to add anymore") {
-                renderHTML();
-                writeToFile(render(employeeInfo));
-            }
-        })
-        .catch(error => {
-            console.log("Was not able to create team-members.html")
-            process.exit(1);
-        });
-}
-
-function renderHTML() {
-    render(employeeInfo);
-}
-
+//add manager info
 function addManager() {
     inquirer.prompt([
         {
             type: "input",
-            name: "manager'",
+            name: "manager",
             message: "What is your manager's name?"
-
         },
         {
-
             type: "input",
             name: "managerId",
             message: "What is your manager's id?"
-
         },
         {
-
             type: "input",
             name: "managerEmail",
             message: "What is your manager's email address?"
-
-
         },
         {
-
             type: "input",
             name: "managerOfficeNumber",
             message: "What is your manager's Office number?"
-
-
         }
     ])
         .then(userInput => {
@@ -95,36 +55,56 @@ function addManager() {
             process.exit(1);
         })
 }
+//add next team members
+function newTeamMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addTeamMember",
+            message: "what type of team member would you like to add",
+            choices: ["Engineer", "Intern", "Done Adding"]
+        }
+    ])
+        .then(userInput => {
+            if (userInput.addTeamMember === "Engineer") {
+                addEngineer();
+            }
+            else if (userInput.addTeamMember === "Intern") {
+                addIntern();
+            }
+            else if (userInput.addTeamMember === "Done Adding") {
+                renderHTML();
+                writeToFile(render(employeeInfo));
+            }
+        })
+        .catch(error => {
+            console.log("Was not able to create team-members.html")
+            process.exit(1);
+        });
+}
+
+//Engineer prompts
 function addEngineer() {
     inquirer.prompt([
         {
             type: "input",
-            name: "engineer'",
+            name: "engineer",
             message: "What is your engineer's name?"
-
         },
         {
-
             type: "input",
             name: "engineerId",
             message: "What is your engineer's id?"
-
         },
         {
-
             type: "input",
             name: "engineerEmail",
             message: "What is your engineer's email address?"
-
-
         },
         {
-
             type: "input",
             name: "engineerGithub",
             message: "What is your engineer's Github username?"
-
-
         }
     ])
         .then(userInput => {
@@ -142,39 +122,28 @@ function addEngineer() {
             process.exit(1);
         })
 }
+//Intern Prompts
 function addIntern() {
     inquirer.prompt([
         {
             type: "input",
             name: "intern",
             message: "What is your intern's name?"
-
-
         },
         {
-
             type: "input",
             name: "internId",
             message: "What is your intern's id?"
-
-
         },
         {
-
             type: "input",
             name: "internEmail",
             message: "What is your intern's email?"
-
-
         },
         {
-
             type: "input",
             name: "internSchool",
             message: "What school does your intern attend?"
-
-
-
         }
     ])
         .then(userInput => {
@@ -194,6 +163,11 @@ function addIntern() {
 }
 
 
+//create HTML file
+function renderHTML() {
+    render(employeeInfo);
+}
+
 function writeToFile(data) {
     return fs.writeFile(outputPath, data, function(error){
         if (error) {
@@ -201,7 +175,7 @@ function writeToFile(data) {
             return;
         }
         
-        console.log("Information completed!")
+        console.log("Team roster completed!")
     });
 
        
@@ -210,25 +184,3 @@ function writeToFile(data) {
 
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
